a0d657bcb708d2644484e10ce711fc1760b05a27
Alexei Karyavin <akaryavin@dataplus.ru>
2022-03-24 01:19:45 +0300

    bug fix

--- assets/libs/createPairs.js
+++ assets/libs/createPairs.js
@@ -3,10 +3,11 @@ import getRandom from './getRandom'
 export default (arr, commissions) => {
   const result = []
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i; j < arr.length - 1; j++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[i] === arr[j]) continue
       result.push({
         base_currency: arr[i],
-        quote_currency: arr[j + 1],
+        quote_currency: arr[j],
         commission: commissions[getRandom(0, commissions.length - 1)],
       })
     }
