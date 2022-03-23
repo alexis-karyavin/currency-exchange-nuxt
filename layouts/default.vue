808f619a80db98d38fffe7156e86f08c93409ccb
Alexei Karyavin <akaryavin@dataplus.ru>
2022-03-23 23:33:50 +0300

    create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div.wrapper-app
+    TheNavBar
+    Nuxt
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style lang="scss">
+#__nuxt,
+#__layout {
+  height: 100%;
+
+  .wrapper-app {
+    height: 100%;
+    background: #f7f7f7;
+  }
+}
+</style>
