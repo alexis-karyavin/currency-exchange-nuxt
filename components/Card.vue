616bc6501aa7f977e6b8283987c14c59c71eb11d
Alexei Karyavin <akaryavin@dataplus.ru>
2022-03-23 23:34:03 +0300

    create card component

---
+++ components/Card.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div.container-card
+    h2.title
+     slot(name="header")
+    slot
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+}
+</script>
+
+<style lang="scss">
+.container-card {
+  max-width: 400px;
+  width: 100%;
+  height: 400px;
+  border: 1px solid #eee;
+  padding: 1rem;
+  background-color: #ffffff;
+
+  h2.title {
+    border-bottom: 1px solid #eee;
+    padding-bottom: 1rem;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,30 @@
-<template lang="pug">
-  div
-    TheNavBar
-    div
-      div {{currencyPairs}}
-      div {{ exchangeRates }}
-    //- button(type='button' ) increment
+<template lang="pug"> 
+  div.content
+    h1.title Crypto Exchange
+
+    div.container-cards
+      Card
+        template(v-slot:header) Exchange EUR to USD
+        div.form-group
+          label You pay
+          input(type="text")
+        div.container-button-reverse  
+          button.btn-reverse aaa
+        div.form-group
+          label You get
+          input(type="text")
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
 import TheNavBar from '@/components/TheNavBar.vue'
+import Card from '@/components/Card.vue'
 
 export default {
   name: 'IndexPage',
   components: {
     TheNavBar,
+    Card,
   },
   computed: {
     ...mapState(['currencyPairs', 'exchangeRates']),
@@ -29,7 +39,31 @@ export default {
 </script>
 
 <style lang="scss">
-button {
-  color: red;
+.content {
+  padding: 1rem;
+
+  .title {
+    margin-bottom: 2rem;
+  }
+
+  .form-group {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      margin-bottom: 0.5rem;
+    }
+  }
+
+  .container-button-reverse {
+    display: flex;
+    justify-content: flex-end;
+    padding-right: 1rem;
+    .btn-reverse {
+      width: 50px;
+      height: 50px;
+      margin-top: 1rem;
+    }
+  }
 }
 </style>
